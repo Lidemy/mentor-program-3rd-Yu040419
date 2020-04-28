8b19f468f9668a5b33e9bceeee8b080d091d963e
Yu <loveboice520@gmail.com>
2020-04-28 16:03:42 +0800

    hw1_1

--- homeworks/week7/hw1/index.js
+++ homeworks/week7/hw1/index.js
@@ -1,12 +1,10 @@
 const minTime = 1000;
 const maxTime = 3000;
 const randomTime = Math.floor(Math.random() * (maxTime - minTime)) + minTime;
-
 let startTime = 0;
 let endTime = 0;
 let playTime = 0;
 let resultTime = 0;
-
 const htmlSelector = document.querySelector('html');
 
 function addBtn() {
@@ -22,11 +20,11 @@ function addBtn() {
 }
 
 function changeColor() {
-  document.querySelector('html').classList.add('bgcolor');
+  htmlSelector.classList.add('bgcolor');
   startTime = new Date();
 }
 
-setTimeout(changeColor, randomTime);
+const timer = window.setTimeout(changeColor, randomTime);
 
 htmlSelector.addEventListener('click', (e) => {
   endTime = new Date();
@@ -40,5 +38,9 @@ htmlSelector.addEventListener('click', (e) => {
     e.stopPropagation();
   } else if (playTime === 1 && startTime === 0) {
     alert('還沒變色喔! 失敗');
+    addBtn();
+    window.clearTimeout(timer);
+    startTime = -1;
+    playTime = 0;
   }
 });
